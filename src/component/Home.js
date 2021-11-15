9ceb21cf2c5f34d667f8b4a0e6fe5d727ed39f8d
Jeremiah <jeremiahmarcos27@outlook.com>
2021-11-15 10:11:49 -0600

    initial commit for jeremiah

---
+++ src/component/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Figure from './Figure'
+
+function Home() {
+
+
+
+    return (
+        <>
+            <div className="main-container">
+                <h1>HOME</h1>
+                <Figure />
+            </div>
+        </>
+    )
+}
+
+export default Home
