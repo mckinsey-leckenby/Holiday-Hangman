dd4aff605e59e4d4b2eee56f3246293bb8fb1e53
DanielSheehan <daniel.sheehan81@yahoo.com>
2021-11-15 16:43:40 -0500

    Added snowflakes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Home from './component/Home';
 import Header from './component/Header';
 import './App.css';
+import Figure from './component/Figure';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Header />
       <Home />
+      <Figure />
     </div>
   );
 }
