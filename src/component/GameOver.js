e76dfb5199642a2dbf1deea48329a917098127bd
Jeremiah <jeremiahmarcos27@outlook.com>
2021-11-18 10:18:51 -0600

    add styling and editted gameover and youwin component

--- src/component/GameOver.js
+++ src/component/GameOver.js
@@ -1,19 +1,31 @@
 import React from 'react'
 import gameOver from "./images/gameOver.jpg"
+import styled from 'styled-components'
 
 function GameOver() {
     let audio2 = new Audio("/losechristmas.mp3")
+    const lost = "https://c.tenor.com/sYx5h9Lzb0MAAAAC/spongebob-sucks-to-suck.gif"
     return (
         <div onClick={audio2.play()}>
-            <h1>Game Over</h1>
-            <h2>YOU TRIED</h2>
+            <Container>YOU THOUGHT</Container>
             <div className="game-over-img">
-                <img src={gameOver} alt="game over" className="results" />
+                <img src={lost} alt="game over" className="results" />
             </div>
 
         </div>
     )
 }
+const Container = styled.h1`
+        font-size: 40px;
+        display: flex;
+        justify-content: space-between;
+        text-align: center;
+        flex-wrap: wrap;
+        top: 100px;
+        color: white;
+        font-family: 'Mountains of Christmas', cursive;
+        padding-top: 4em;
+        text-shadow: 2px 2px #ff0000;`
 
 export default GameOver
 
--- src/component/YouWin.js
+++ src/component/YouWin.js
@@ -1,18 +1,31 @@
 import React from 'react'
 import win from "./images/win.jpg"
+import styled from 'styled-components'
 
 function YouWin({ status }) {
     let audio3 = new Audio("/winchristmas.mp3")
 
-    const imageWin = "https://cdn5.vectorstock.com/i/1000x1000/69/19/congratulations-christmas-banner-with-bows-vector-21146919.jpg"
+    const imageWin = "https://media0.giphy.com/media/1GTZA4flUzQI0/giphy.gif"
 
     return (
         <div onClick={audio3.play()} >
             {/* {status === "win" && audio3.play()} */}
-            <h1>YOU WIN</h1>
-            <img src={win} alt="you win" className="results" />
+            <Container>YOU WIN</Container>
+            <img src={imageWin} alt="you win" className="results" />
         </div>
     )
 }
+
+const Container = styled.h1`
+        font-size: 40px;
+        display: flex;
+        justify-content: space-between;
+        text-align: center;
+        flex-wrap: wrap;
+        top: 100px;
+        color: white;
+        font-family: 'Mountains of Christmas', cursive;
+        padding-top: 4em;
+        text-shadow: 2px 2px #ff0000;`
 
 export default YouWin
